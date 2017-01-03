55e8b6156514c0b009c3141d09f0c3fd0a22ca9c
Sasa Dobrilovic <thespazzj@gmail.com>
2017-01-03 23:36:54 +0100

    added association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-  
+  has_many :articles
+  before_save { self.email = email.downcase }
+  validates :username, presence: true, uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+            length: { maximum: 105 }
+
 end
